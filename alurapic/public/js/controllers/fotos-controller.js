078406631a52d48daa490e9fdea755c5463682b4
JacksonPassos <jacksonguitar33@gmail.com>
2017-05-10 07:13:45 -0300

    enviando arquivos

---
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,21 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+	$scope.fotos = [
+		{
+			titulo: "Guitar",
+			url: "http://www.namoral.com.br/blog/wp-content/uploads/2011/05/guitarra-02.jpg"
+		},
+		{
+			titulo: "Jimi Hendrix",
+			url: "http://www.vandohalen.com.br/wp-content/uploads/2013/11/jimi-hendrix-lovers-4131.jpg"
+		},
+		{
+			titulo: "John Frusciante",
+			url: "https://pxhst.co/avaxhome/d4/40/002940d4.jpeg"
+		},
+		{
+			titulo: "Pink Floyd",
+			url: "http://www.rockcabeca.com/wp-content/uploads/2016/12/image002.jpg"
+		}
+	]
+});
